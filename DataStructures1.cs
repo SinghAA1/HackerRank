7b6468540ad64dea93b6fccfd08540e80841d5a9
SinghAA1 <A.A.Singh1@lse.ac.uk>
2018-01-02 23:57:07 +0000

    My First Commit

---
+++ DataStructures1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Test
+{
+    class MainClass
+    {
+        /*
+         * Reverse then Print, Array of Ints
+        */
+        public static void Main(string[] args)
+        {
+            Console.ReadLine();
+            string[] arr_temp = Console.ReadLine().Split(' ');
+            int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+
+            if(arr != null && arr.Count()>0)
+            {
+				var numberList = arr.ToList(); 
+				numberList.Reverse();
+				string output = string.Empty; 
+				numberList.ForEach(number =>
+				{
+					output += string.Format("{0} ", number);
+				});
+				output = output.TrimEnd();
+				Console.WriteLine(output);
+				Console.ReadKey();
+            }
+
+        }
+    }
+}
